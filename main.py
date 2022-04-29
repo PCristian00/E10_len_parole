4e95700ecde91257e3f90e3fed93eda85e9b0577
PCristian00 <cristianp9300@gmail.com>
2022-04-29 17:50:46 +0200

    Aggiunta versione singola riga

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def contatore(list_a):
     list_b = []
+    list_c=[len(p) for p in list_a]
     for parola in list_a:
         list_b.append(len(parola))
     print(list_b)
-
+    print(list_c)
 
 lista = ['Milano', 'Roma', 'Cagliari', 'Croazia', 'Giappone']
 print(lista)
